84f7d48951d2574ea23df6bb24168e7de9b1f9e6
Andrey <ae.nalimov@mail.ru>
2024-04-09 01:02:03 +0300

    refactor: add comments

--- app/src/main/java/com/nonoxy/d2buildhelper/domain/repository/ResourceClient.kt
+++ app/src/main/java/com/nonoxy/d2buildhelper/domain/repository/ResourceClient.kt
@@ -1,9 +1,19 @@
 package com.nonoxy.d2buildhelper.domain.repository
 
 interface ResourceClient {
+
+    // -> {itemId: shortName}
     suspend fun getItemNameById(itemIds: List<Int>): MutableMap<Short, String>
+
+    // -> {heroId: {displayName: displayName, shortName: shortName} }
     suspend fun getHeroNameById(heroIds: List<Int>): MutableMap<Short, MutableMap<String, String>>
+
+    // -> URL
     suspend fun getHeroImageUrlByName(heroName: String): String
+
+    // -> URL
     suspend fun getItemImageUrlByName(itemName: String): String
+
+    // -> URL
     suspend fun getUtilImageUrlByName(utilName: String): String
 }
