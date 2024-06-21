336a1d46249cf9d815c072b950f4980065c50347
Andrey <ae.nalimov@mail.ru>
2024-06-21 22:59:48 +0300

    feat: add service locator

--- composeApp/src/commonMain/kotlin/dev/nonoxy/d2buildhelper/core/di/InjectProvider.kt
+++ composeApp/src/commonMain/kotlin/dev/nonoxy/d2buildhelper/core/di/InjectProvider.kt
@@ -2,31 +2,33 @@ package dev.nonoxy.d2buildhelper.core.di
 
 import Dota___Build_Helper.composeApp.BuildConfig
 import com.apollographql.apollo3.ApolloClient
+import kotlin.reflect.KClass
 
 object InjectProvider {
     private val dependencies: HashMap<String, Any> = hashMapOf()
 
     init {
-        addDependency(
-            key = DependencyKey.ApolloClient.title,
-            dependency = ApolloClient.Builder()
-                .serverUrl(BuildConfig.API_BASE_URL)
-                .addHttpHeader("Authorization", "Bearer ${BuildConfig.API_KEY}")
-                .build()
-        )
+        addDependency(type = ApolloClient::class, dependency = createApolloClient())
     }
 
-    fun addDependency(key: String, dependency: Any) {
-        dependencies[key] = dependency
+    fun addDependency(type: KClass<*>, dependency: Any) {
+        if (type.qualifiedName == null)
+            throw IllegalArgumentException("Local and anonymous can not be added")
+
+        if (!dependencies.contains(type.qualifiedName)) {
+            dependencies[type.qualifiedName!!] = dependency
+        }
     }
 
-    fun <T> getDependency(key: String): T {
-        return dependencies[key] as T
+    fun <T> getDependency(type: KClass<*>): T {
+        @Suppress("UNCHECKED_CAST")
+        return dependencies[type.qualifiedName] as T
     }
 
-    fun getApolloClient(): ApolloClient = getDependency(DependencyKey.ApolloClient.title)
-}
-
-private enum class DependencyKey(val title: String) {
-    ApolloClient("apolloClient"),
+    private fun createApolloClient(): ApolloClient {
+        return ApolloClient.Builder()
+            .serverUrl(BuildConfig.API_BASE_URL)
+            .addHttpHeader("Authorization", "Bearer ${BuildConfig.API_KEY}")
+            .build()
+    }
 }
