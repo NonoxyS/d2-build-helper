fb92367cbf83f7b60fe53e0f36b75f335b0e5ec3
Andrey <ae.nalimov@mail.ru>
2024-06-26 05:28:23 +0300

    feat: add custom theme implementation

---
+++ composeApp/src/commonMain/kotlin/dev/nonoxy/d2buildhelper/theme/D2BuildHelperColors.kt
@@ -0,0 +1,23 @@
+package dev.nonoxy.d2buildhelper.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+
+data class D2BuildHelperColors(
+    val primaryText: Color,
+    val primaryBackground: Color,
+    val primaryContainer: Color,
+    val secondaryText: Color,
+    val secondaryBackground: Color,
+    val tintColor: Color,
+)
+
+object D2BuildHelperTheme {
+    val colors: D2BuildHelperColors
+        @Composable
+        get() = LocalD2BuildHelperColor.current
+}
+
+val LocalD2BuildHelperColor =
+    staticCompositionLocalOf<D2BuildHelperColors> { error("No default implementation for colors") }
