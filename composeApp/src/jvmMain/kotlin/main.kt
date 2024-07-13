85e090706da836d49735cbd68fccd3e96b91c5a7
Andrey <ae.nalimov@mail.ru>
2024-07-13 04:54:30 +0300

    change app name

--- composeApp/src/jvmMain/kotlin/main.kt
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -2,12 +2,15 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import java.awt.Dimension
 import dev.nonoxy.d2buildhelper.App
+import dota_2_build_helper.composeapp.generated.resources.Res
+import dota_2_build_helper.composeapp.generated.resources.app_name
+import org.jetbrains.compose.resources.stringResource
+import java.awt.Dimension
 
 fun main() = application {
     Window(
-        title = "Dota 2 Build Helper",
+        title = stringResource(Res.string.app_name),
         state = rememberWindowState(width = 800.dp, height = 600.dp),
         onCloseRequest = ::exitApplication,
     ) {
