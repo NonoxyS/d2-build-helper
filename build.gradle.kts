bea2703a502dc1d0b1bafe3f80a656eee61f69ff
Andrey Nalimov <ae.nalimov@gmail.com>
2026-05-17 17:31:57 +0300

    chore: wire detekt at root and apply android lint configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,43 @@ plugins {
     alias(libs.plugins.buildConfig).apply(false)
     alias(libs.plugins.kotlinx.serialization).apply(false)
     alias(libs.plugins.apollo).apply(false)
+
+    alias(libs.plugins.detekt)
+}
+
+apply(from = "$rootDir/linters/androidLint/lintConfiguration.gradle")
+
+dependencies {
+    detektPlugins(libs.detekt.formatting)
+    detektPlugins(libs.detekt.compose.kode)
+    detektPlugins(libs.detekt.compose.twitter)
+}
+
+detekt {
+    disableDefaultRuleSets = true
+    buildUponDefaultConfig = true
+    autoCorrect = true
+    description = "Detekt with formatting."
+    baseline = file("$rootDir/linters/detekt/baseline.xml")
+    config.setFrom(files("$rootDir/linters/detekt/config.yml"))
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    setSource(files(projectDir))
+    include("**/*.kt")
+    include("**/*.kts")
+    exclude("**/resources/**")
+    exclude("**/build/**")
+    exclude("**/.gradle/**")
+
+    reports {
+        html {
+            required.set(true)
+            outputLocation.set(file("$rootDir/build/reports/detekt/detekt.html"))
+        }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.layout.buildDirectory)
 }
