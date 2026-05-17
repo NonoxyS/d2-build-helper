0d4ae01e5008cc935b5fec54168464b2e4d1a011
Andrey Nalimov <ae.nalimov@gmail.com>
2026-05-18 02:05:44 +0300

    feat: initialize Napier (DebugAntilog) on Android, JVM and iOS entry points

--- composeApp/src/iosMain/kotlin/main.kt
+++ composeApp/src/iosMain/kotlin/main.kt
@@ -1,10 +1,15 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import dev.nonoxy.d2buildhelper.App
 import dev.nonoxy.d2buildhelper.core.di.initKoin
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
 import platform.UIKit.UIViewController
 
 fun MainViewController(): UIViewController = ComposeUIViewController(
-    configure = { initKoin() }
+    configure = {
+        Napier.base(DebugAntilog(defaultTag = "D2BuildHelper"))
+        initKoin()
+    }
 ) {
     App()
 }
