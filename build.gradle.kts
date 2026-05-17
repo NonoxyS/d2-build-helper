8480e2c7826cfa309f1595b37bc33f1ad977e7c5
Andrey Nalimov <ae.nalimov@gmail.com>
2026-05-17 17:50:15 +0300

    chore(detekt): wire baseline task source paths and record initial baseline

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,19 +21,23 @@ dependencies {
 detekt {
     disableDefaultRuleSets = true
     buildUponDefaultConfig = true
-    autoCorrect = true
+    autoCorrect = false
     description = "Detekt with formatting."
     baseline = file("$rootDir/linters/detekt/baseline.xml")
     config.setFrom(files("$rootDir/linters/detekt/config.yml"))
 }
 
-tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+fun SourceTask.setupDetektFolders() {
     setSource(files(projectDir))
     include("**/*.kt")
     include("**/*.kts")
     exclude("**/resources/**")
     exclude("**/build/**")
     exclude("**/.gradle/**")
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    setupDetektFolders()
 
     reports {
         html {
@@ -41,6 +45,10 @@ tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
             outputLocation.set(file("$rootDir/build/reports/detekt/detekt.html"))
         }
     }
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.DetektCreateBaselineTask>().configureEach {
+    setupDetektFolders()
 }
 
 tasks.register("clean", Delete::class) {
