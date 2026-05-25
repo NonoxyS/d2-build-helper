f887a923c5e803879f9d7c61047a94d71e271af8
Andrey Nalimov <ae.nalimov@gmail.com>
2026-05-25 19:15:51 +0300

    chore(di): unify Koin bindings to *Of(::Impl) { bind<T>() }
    CommonModule and CoreResourcesModule switch verbose `single<T> { Impl(...) }`
    declarations to constructor-driven `singleOf(::Impl) { bind<T>() }` /
    `factoryOf(::Impl) { bind<T>() }`. Same wiring, less boilerplate, and a
    single canonical syntax across modules.

--- shared/common/src/commonMain/kotlin/dev/nonoxy/d2buildhelper/common/di/CommonModule.kt
+++ shared/common/src/commonMain/kotlin/dev/nonoxy/d2buildhelper/common/di/CommonModule.kt
@@ -2,8 +2,10 @@ package dev.nonoxy.d2buildhelper.common.di
 
 import dev.nonoxy.d2buildhelper.common.coroutines.CoroutineDispatchers
 import dev.nonoxy.d2buildhelper.common.coroutines.CoroutineDispatchersImpl
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val commonModule = module {
-    single<CoroutineDispatchers> { CoroutineDispatchersImpl() }
+    singleOf(::CoroutineDispatchersImpl) { bind<CoroutineDispatchers>() }
 }
--- shared/core-resources/src/commonMain/kotlin/dev/nonoxy/d2buildhelper/core/resources/di/CoreResourcesModule.kt
+++ shared/core-resources/src/commonMain/kotlin/dev/nonoxy/d2buildhelper/core/resources/di/CoreResourcesModule.kt
@@ -6,14 +6,13 @@ import dev.nonoxy.d2buildhelper.core.resources.data.repository.ConstantsStorageI
 import dev.nonoxy.d2buildhelper.core.resources.data.repository.ResourcesRepositoryImpl
 import dev.nonoxy.d2buildhelper.core.resources.domain.repository.ConstantsStorage
 import dev.nonoxy.d2buildhelper.core.resources.domain.repository.ResourcesRepository
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val coreResourcesModule = module {
-    factory<ConstantsApiClient> { ConstantsApiClientImpl(ktorClient = get()) }
-    single<ConstantsStorage> {
-        ConstantsStorageImpl(dataStore = get(), dispatchers = get(), json = get())
-    }
-    single<ResourcesRepository> {
-        ResourcesRepositoryImpl(apiClient = get(), storage = get(), dispatchers = get())
-    }
+    factoryOf(::ConstantsApiClientImpl) { bind<ConstantsApiClient>() }
+    singleOf(::ConstantsStorageImpl) { bind<ConstantsStorage>() }
+    singleOf(::ResourcesRepositoryImpl) { bind<ResourcesRepository>() }
 }
