ad59c24901fbbf999e10188ba31af1a2f65911cb
Andrey Nalimov <ae.nalimov@gmail.com>
2026-05-26 11:13:18 +0300

    feat(common-ui): apply window insets inside D2TopBar

--- shared/common-ui/src/commonMain/kotlin/dev/nonoxy/d2buildhelper/common/ui/compose/components/shared/topbar/D2TopBar.kt
+++ shared/common-ui/src/commonMain/kotlin/dev/nonoxy/d2buildhelper/common/ui/compose/components/shared/topbar/D2TopBar.kt
@@ -3,9 +3,12 @@ package dev.nonoxy.d2buildhelper.common.ui.compose.components.shared.topbar
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -24,6 +27,7 @@ fun D2TopBar(
     titleColor: Color = D2BuildHelperTheme.colors.textPrimary,
     height: Dp = 56.dp,
     contentPadding: PaddingValues = PaddingValues(horizontal = 16.dp),
+    windowInsets: WindowInsets = WindowInsets.statusBars,
     horizontalArrangement: Arrangement.Horizontal = Arrangement.SpaceBetween,
     leading: (@Composable () -> Unit)? = null,
     trailing: (@Composable () -> Unit)? = null,
@@ -31,6 +35,7 @@ fun D2TopBar(
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .windowInsetsPadding(windowInsets)
             .height(height)
             .padding(contentPadding),
         verticalAlignment = Alignment.CenterVertically,
