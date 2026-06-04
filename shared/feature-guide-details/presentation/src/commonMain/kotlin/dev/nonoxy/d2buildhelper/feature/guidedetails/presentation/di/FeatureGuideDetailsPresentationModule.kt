2812783a9cf342125087582b24d800b18c0c7ea0
Andrey Nalimov <ae.nalimov@gmail.com>
2026-06-05 00:06:50 +0300

    feat(feature-guide-details): add ViewModel and presentation Koin

--- shared/feature-guide-details/presentation/src/commonMain/kotlin/dev/nonoxy/d2buildhelper/feature/guidedetails/presentation/di/FeatureGuideDetailsPresentationModule.kt
+++ shared/feature-guide-details/presentation/src/commonMain/kotlin/dev/nonoxy/d2buildhelper/feature/guidedetails/presentation/di/FeatureGuideDetailsPresentationModule.kt
@@ -1,14 +1,20 @@
 package dev.nonoxy.d2buildhelper.feature.guidedetails.presentation.di
 
+import dev.nonoxy.d2buildhelper.feature.guidedetails.presentation.GuideDetailViewModel
 import dev.nonoxy.d2buildhelper.feature.guidedetails.presentation.mappers.UiGuideDetailLabelMapper
 import dev.nonoxy.d2buildhelper.feature.guidedetails.presentation.mappers.UiGuideDetailLabelMapperImpl
 import dev.nonoxy.d2buildhelper.feature.guidedetails.presentation.mappers.UiGuideDetailStateMapper
 import dev.nonoxy.d2buildhelper.feature.guidedetails.presentation.mappers.UiGuideDetailStateMapperImpl
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModel
+import org.koin.core.parameter.parametersOf
 import org.koin.dsl.module
 
 val featureGuideDetailsPresentationModule = module {
     singleOf(::UiGuideDetailStateMapperImpl) { bind<UiGuideDetailStateMapper>() }
     singleOf(::UiGuideDetailLabelMapperImpl) { bind<UiGuideDetailLabelMapper>() }
+    viewModel { (matchId: Long, steamAccountId: Long) ->
+        GuideDetailViewModel(get { parametersOf(matchId, steamAccountId) }, get(), get())
+    }
 }
